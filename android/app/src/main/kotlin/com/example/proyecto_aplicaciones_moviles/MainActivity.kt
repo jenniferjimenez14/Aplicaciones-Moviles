be936a31a9b714c96f9973e0ad69ec1536354351
Jennifer Elissette Jimenez Vera <jennifelissette@hotmail.com>
2021-05-02 14:20:36 -0500

    Examen Leonardo

---
+++ android/app/src/main/kotlin/com/example/proyecto_aplicaciones_moviles/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_aplicaciones_moviles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_aplicaciones_moviles/icono_string_util.dart';
+import 'package:proyecto_aplicaciones_moviles/provider_menu.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proyecto Departamental',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Proyecto Departamental"),
+      ),
+      body: _lista(context),
+    );
+  }
+
+  Widget _lista(BuildContext context) {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(children: _listItems(snapshot.data, context));
+      },
+    );
+  }
+
+  List<Widget> _listItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) =>
+                _buildPopUpDialog(context, opt['texto']),
+          );
+        },
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+    return opciones;
+  }
+
+  Widget _buildPopUpDialog(BuildContext context, String opt) {
+    return new AlertDialog(
+      title: const Text('diste click en la opcion '),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('hola soy la opcion :' + opt),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColorDark,
+          child: const Text('cerrar'),
+        ),
+      ],
+    );
+  }
+}
